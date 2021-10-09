7cf2937e176a2094a59279c8823626a9a1c237be
Tanmoy Sarkar <ts741127@gmail.com>
2021-10-10 00:08:42 +0530

    tawk.to added

--- user_dashboard/static/user_dashboard/js/soft-ui-dashboard.js
+++ user_dashboard/static/user_dashboard/js/soft-ui-dashboard.js
@@ -396,15 +396,18 @@ window.addEventListener("resize", navbarColorOnResize);
 
 function navbarColorOnResize() {
   if (window.innerWidth > 1200) {
-    if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
-      sidenav.classList.remove('bg-white');
+    if (referenceButtons && referenceButtons != undefined){
+      if (referenceButtons.classList.contains('active') && referenceButtons.getAttribute('data-class') === 'bg-transparent') {
+        sidenav.classList.remove('bg-white');
+      } else {
+        sidenav.classList.add('bg-white');
+      }
     } else {
       sidenav.classList.add('bg-white');
+      sidenav.classList.remove('bg-transparent');
     }
-  } else {
-    sidenav.classList.add('bg-white');
-    sidenav.classList.remove('bg-transparent');
-  }
+    }
+
 }
 
 // Deactivate sidenav type buttons on resize and small screens
